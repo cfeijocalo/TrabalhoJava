a8cccb08d46ce9f551b82b1e343e44afeebc56d9
Caio Calo <caio.calo@vetorlog.com>
2017-09-27 14:38:13 -0300

    consertos

--- src/org/ufpr/sistemapedidos/controller/PedidoViewController.java
+++ src/org/ufpr/sistemapedidos/controller/PedidoViewController.java
@@ -13,13 +13,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonBar.ButtonData;
 
 /**
  * 
@@ -50,12 +48,6 @@ public class PedidoViewController {
 	
 	@FXML
 	private TableColumn<ItemDoPedido, String> produtoColumn;
-	
-	@FXML
-	private TextField pesquisaField;
-
-	@FXML
-	private DatePicker dataPesquisa;
 	
 	@FXML
 	private Label nPedidoLabel;
